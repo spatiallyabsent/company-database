874c96ec25884f42352435b7f0daa256c983ce49
SpatiallyAbsent <joshuapdow@gmail.com>
2024-01-27 15:12:02 -0700

    added post and get/id to tag-routes.js. need to add more into models Tag.js before going onto put/delete functions

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -17,22 +17,29 @@ router.get('/', async (req, res) => {
   }
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
   try {
-    
+    const tagId = await Tag.findByPk(req.params.id, { include:[Product] })
+    if (!tagId) {
+      res.status(404).json({ message: 'No tags found with that ID'});
+      return;
+    }
+    res.status(200).json(tagId)
   } catch (error) {
-    
+    res.status(500).json(error);
   }
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new tag
   try {
+    const newTag = await Tag.create(req.body);
+    res.status(201).json(newTag);
     
   } catch (error) {
-    
+    res.status(400).json(error);
   }
 });
 
